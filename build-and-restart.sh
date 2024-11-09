97494deaf68f119034ac7f8879976a48abe4c3b3
Piotr <ptrkasiewicz@gmail.com>
2024-11-09 20:36:14 +0100

    change port in build-and-restart.sh

--- build-and-restart.sh
+++ build-and-restart.sh
@@ -1,4 +1,4 @@
 docker build -t psa .
 docker stop psa || true
 docker rm psa || true
-docker run -d -p 8081:8080 --name=psa --network dbr-network --restart unless-stopped psa
+docker run -d -p 8082:8080 --name=psa --network dbr-network --restart unless-stopped psa
